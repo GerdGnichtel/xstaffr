b642e3a670ec6b0581b654570b94a336bde8c72d
Gnichtel <Gerd.Gnichtel@opitz-consulting.com>
2018-07-15 17:28:00 +0200

    xstaffr initiales Maven Setup mit Log4j und 3 Modulen - client.fx - common - report

---
+++ client.fx/src/main/java/de/oc/xstaffr/client/fx/XStaffrAppRunner.java
@@ -0,0 +1,12 @@
+package de.oc.xstaffr.client.fx;
+
+import org.apache.log4j.Logger;
+
+public class XStaffrAppRunner {
+	
+	final static Logger logger = Logger.getLogger(XStaffrAppRunner.class);
+	
+	public static void main(String[] args) { 
+		logger.debug("Hello world!");
+	}
+}
